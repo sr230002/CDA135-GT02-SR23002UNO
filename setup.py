62e7ff57d47629cd560ab9a6cd9a4a064e80e8c4
David Sixco <sr23002@ues.edu.sv>
2025-04-12 23:09:55 -0600

    chore: update version library

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name='SR23002UNO',
-    version='1.0.4',
+    version='1.0.5',
     author='David Enrique Sixco Ramos',
     author_email='sr23002@ues.edu.sv',
     description='Liberia para resolver ecuaciones lineales y no lineales',
